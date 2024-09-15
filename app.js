9191f1df8c5426783b918f54781deeece360d1e1
Tapu45 <rameswarpanda891@gmail.com>
2024-09-15 10:09:41 +0530

    cors

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    origin: ["http://localhost:5173", process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
